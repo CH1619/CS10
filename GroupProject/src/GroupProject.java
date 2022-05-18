d37cea29a3dcfdf39544119c111bbd8d6a97be06
16192005 <16192005@L815WD220034.edu.cbe.ab.ca>
2022-05-18 13:29:11 -0600

    Updating

--- GroupProject/src/GroupProject.java
+++ GroupProject/src/GroupProject.java
@@ -1,8 +1,70 @@
 
-public class GroupProject {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class GroupProject 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner userinput = new Scanner(System.in);
+	
+		
+		int randomnum1 = 1 + (int) (Math.random() * 10);
+	
+		int randomnum2 = 1 + (int) (Math.random() * 10);
+	
+		System.out.println("Select an operator by typing the associated number: ");
+	
+		System.out.println("1 : Addition"); 
+		System.out.println("2 : Subtraction");
+		System.out.println("3 : Division");
+		System.out.println("4 : Multiplication");
+	
+		int operator = userinput.nextInt();
+		
+		double answer = 0;
+		
+		double Canswer = 0;
+		
+		switch (operator)
+		{
+			case 1:
+				Canswer = randomnum1 + randomnum2;
+				System.out.println("What is " + randomnum1 + "/" + randomnum2 + "?");
+				answer = userinput.nextInt();
+			
+				break;
+	
+			case 2:
+				Canswer = randomnum1 - randomnum2;
+				System.out.println("What is " + randomnum1 + "*" + randomnum2 + "?");
+				answer = userinput.nextInt();
+			
+				break;
+	
+			case 3:
+				Canswer = randomnum1/randomnum2;
+				System.out.println("What is " + randomnum1 + "-" + randomnum2 + "?");
+				answer = userinput.nextInt();
+			
+				break;
+	
+			case 4:
+				Canswer = randomnum1*randomnum2;
+				System.out.println("What is " + randomnum1 + "+" + randomnum2 + "?");
+				answer = userinput.nextInt();
+			
+				 break;
+		}
+		
+		if (answer != Canswer)
+		{
+			System.out.println("You got it right! Congradulations!!");
+		}
+		else
+		{
+			System.out.println("you were incorrect. You'll get it next time!");
+		}	
 
 	}
 
